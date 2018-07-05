289e4c6bee0a0c62630e72037f044c05f21c4dd8
Vibhav Bobade <vibhav.bobde@gmail.com>
2018-07-05 18:16:22 +0530

    Addition : Added unit tests for CrawlWebsite and GetHTTPRequest

--- cmd/crawl_test.go
+++ cmd/crawl_test.go
@@ -1,17 +1,55 @@
 package cmd
 
 import (
+	"net/http"
 	"testing"
 )
 
+var tt = []struct {
+	name                 string
+	website              string
+	concurrentGoroutines int
+}{
+	{"No arguments and 0 goroutines", "", 0},
+	{"Wrong argument and 1 goroutine", "webscraper", 0},
+	{"Website and minimum goroutines", "http://www.guimp.com/", 2},
+	{"Website and lot of goroutines", "webscraper.io", 300},
+}
+
 func TestCrawlWebsite(t *testing.T) {
-	var urls = []string{
-		"webscraper.io",
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			err := CrawlWebsite(tc.website, tc.concurrentGoroutines)
+			if err != nil {
+				t.Fatalf("\n Error during testing %v ", err)
+			}
+		})
 	}
-	for _, u := range urls {
-		err := CrawlWebsite(u, 100)
-		if err != nil {
-			t.Fail()
-		}
+}
+
+var ttReq = []struct {
+	name    string
+	website string
+}{
+	{"Guimp.Com", "http://www.guimp.com/"},
+	{"Webscraper.io", "http://webscraper.io"},
+}
+
+func TestGetHTTPGETRequest(t *testing.T) {
+	for _, tc := range ttReq {
+		t.Run(tc.name, func(t *testing.T) {
+			var req = getHTTPGETRequest(tc.website)
+			httpClient := getHTTPClient()
+
+			res, err := httpClient.Do(req)
+			if err != nil {
+				t.Fatalf("Could not read response from site %v : %v \n", tc.website, err)
+			}
+			defer res.Body.Close()
+
+			if res.StatusCode != http.StatusOK {
+				t.Errorf("Expected Status OK : got %v", res.Status)
+			}
+		})
 	}
 }
